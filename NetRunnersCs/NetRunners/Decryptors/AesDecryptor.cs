456d7662fe454f897695b6238231c093d5abe3cd
gustanini.gustanini <pimentelra10@gmail.com>
2024-04-18 13:23:46 +0200

    update aesdecryptor to support older .net

--- NetRunnersCs/NetRunners/Decryptors/AesDecryptor.cs
+++ NetRunnersCs/NetRunners/Decryptors/AesDecryptor.cs
@@ -16,41 +16,47 @@ namespace NetRunners.Decryptors
         /// <param name="EncryptedBytes"></param>
         /// <param name="AesKey"></param>
         /// <returns>Byte[]</returns>
-        public static byte[] DecryptBytesToBytesAes(byte[] EncryptedBufferWithIV, byte[] AesKey)
+    public static byte[] DecryptBytesToBytesAes(byte[] EncryptedBufferWithIV, byte[] AesKey)
+    {
+        // Assuming IV is always the first 16 bytes of the encrypted buffer
+        byte[] AesIV = new byte[16]; // AES IV size is 128 bits (16 bytes)
+        // Extract the IV from the beginning of the encrypted buffer
+        Array.Copy(EncryptedBufferWithIV, 0, AesIV, 0, AesIV.Length);
+
+        // The actual encrypted data starts after the IV
+        byte[] EncryptedBuffer = new byte[EncryptedBufferWithIV.Length - AesIV.Length];
+        Array.Copy(EncryptedBufferWithIV, AesIV.Length, EncryptedBuffer, 0, EncryptedBuffer.Length);
+
+        using (AesManaged aes = new AesManaged())
         {
-            // Assuming IV is always the first 16 bytes of the encrypted buffer
-            byte[] AesIV = new byte[16]; // AES IV size is 128 bits (16 bytes)
-            // Extract the IV from the beginning of the encrypted buffer
-            Array.Copy(EncryptedBufferWithIV, 0, AesIV, 0, AesIV.Length);
+            aes.Key = AesKey;
+            aes.IV = AesIV;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7; // This should match the padding used in encryption
 
-            // The actual encrypted data starts after the IV
-            byte[] EncryptedBuffer = new byte[EncryptedBufferWithIV.Length - AesIV.Length];
-            Array.Copy(EncryptedBufferWithIV, AesIV.Length, EncryptedBuffer, 0, EncryptedBuffer.Length);
+            ICryptoTransform aes_decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
 
-            using (AesManaged aes = new AesManaged())
+            using (MemoryStream Encryptedms = new MemoryStream(EncryptedBuffer))
             {
-                aes.Key = AesKey;
-                aes.IV = AesIV;
-                aes.Mode = CipherMode.CBC;
-                aes.Padding = PaddingMode.PKCS7; // This should match the padding used in encryption
-
-                ICryptoTransform aes_decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                using (MemoryStream Encryptedms = new MemoryStream(EncryptedBuffer))
+                using (MemoryStream Decryptedms = new MemoryStream())
                 {
-                    using (MemoryStream Decryptedms = new MemoryStream())
+                    using (CryptoStream cs = new CryptoStream(Encryptedms, aes_decryptor, CryptoStreamMode.Read))
                     {
-                        using (CryptoStream cs = new CryptoStream(Encryptedms, aes_decryptor, CryptoStreamMode.Read))
+                        const int blockSize = 4096;
+                        byte[] buffer = new byte[blockSize];
+                        int bytesRead;
+                        while ((bytesRead = cs.Read(buffer, 0, buffer.Length)) > 0)
                         {
-                            cs.CopyTo(Decryptedms);
+                            Decryptedms.Write(buffer, 0, bytesRead);
                         }
-
-                        byte[] decrypted_buff = Decryptedms.ToArray();
-                        return decrypted_buff;
                     }
+
+                    byte[] decrypted_buff = Decryptedms.ToArray();
+                    return decrypted_buff;
                 }
             }
         }
+    }
         /// <summary>
         /// This Function Takes in an AES Encrypted Buffer and The Key/IV Used in Encryption and Returns The Decrypted Buffer
         /// </summary>
